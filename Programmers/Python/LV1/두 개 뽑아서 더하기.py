6cc0f27ef261a3e6a7df225cc4167f9a0f53abef
DESKTOP-CVFGNRF\jlppo <jlppo@naver.com>
2020-09-21 18:16:51 +0900

    something lv.1

--- Programmers/Python/LV1/두 개 뽑아서 더하기.py
+++ Programmers/Python/LV1/두 개 뽑아서 더하기.py
@@ -4,14 +4,15 @@ def solution(numbers):
     for i in range(0, int(len(numbers))):
         for j in range(1, int(len(numbers)-1)):
             new.append(int(numbers[i-j]) + int(numbers[i]))
-    new.sort()
+            
     new = list(set(new))
+    new.sort()
     return new
 
 print(solution([2,1,3,4,1]))
 print(solution([5,0,2,7]))
 print(solution([1,2,3]))
-
+print(solution([7,3]))
 # for i in range(0, int(len(numbers))): # 1칸
 #     new.append(int(numbers[i-1]) + int(numbers[i]))
 
@@ -23,18 +24,18 @@ print(solution([1,2,3]))
 
 # for i in range(0, int(len(numbers))): # 4칸
 #     new.append(int(numbers[i-1]) + int(numbers[i]))
-new = []
-numbers = [1, 2, 3]
-i = int(len(numbers))
-while i >= 0:
-    for i in range(int(len(numbers)), 0): # 1칸
-        new.append(int(numbers[i]) + int(numbers[i+1]))
+# new = []
+# numbers = [1, 2, 3]
+# i = int(len(numbers))
+# while i >= 0:
+#     for i in range(int(len(numbers)), 0): # 1칸
+#         new.append(int(numbers[i]) + int(numbers[i+1]))
 
     
-        i -= 1
-    break
-new.sort()
-new = list(set(new))
-    # return new
+#         i -= 1
+#     break
+# new.sort()
+# new = list(set(new))
+#     # return new
 
-print(new)
+# print(new)
