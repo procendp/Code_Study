c1bd1a82ae53345ddea09a282a6ff65529598a59
DESKTOP-CVFGNRF\procendp <procendp@gmail.com>
2020-10-06 22:27:29 +0900

    modified some

--- BaekJoon/Python/맞은사람순/27____설탕 배달.py
+++ BaekJoon/Python/맞은사람순/27____설탕 배달.py
@@ -22,25 +22,27 @@ if N % 5 != 0 and N % 3 != 0:
     print(-1)
 
 
+
+
 # 마지막 테케 11 해결 합시다..........
 
 
 
 
 
-# if N % 5 == 0 or N % 3 == 0:
-#     if N % 5 == 0:
-#         num.append(N // 5)
-#         num.append((N // 5) // 3)
-#         num_list = list(map(int, num))
-#         total = sum(num_list)
-#         print(total)
-#     elif N % 5 != 0:
-#         print(N // 3)
+if N % 5 == 0 or N % 3 == 0:
+    if N % 5 == 0:
+        num.append(N // 5)
+        num.append((N // 5) // 3)
+        num_list = list(map(int, num))
+        total = sum(num_list)
+        print(total)
+    elif N % 5 != 0:
+        print(N // 3)
 
 
-# elif N % 5 != 0 and N % 3 != 0:
-#     print(-1)
+elif N % 5 != 0 and N % 3 != 0:
+    print(-1)
 
 
 
