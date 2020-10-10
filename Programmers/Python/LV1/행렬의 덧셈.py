3a00a12f2f21eef954cee457a2d43992feac39be
DESKTOP-CVFGNRF\procendp <procendp@gmail.com>
2020-10-10 23:10:42 +0900

    sum of matrix

---
+++ Programmers/Python/LV1/행렬의 덧셈.py
@@ -0,0 +1,12 @@
+def solution(arr1, arr2):
+    answer = []
+    for i in range(len(arr1)):
+        temp = []
+        for j in range(len(arr1[0])):
+            temp.append(arr1[i][j] + arr2[i][j])
+        answer.append(temp)
+
+    return answer
+
+print(solution([[1,2],[2,3]], [[3,4],[5,6]]))
+print(solution([[1],[2]], [[3],[4]]))
