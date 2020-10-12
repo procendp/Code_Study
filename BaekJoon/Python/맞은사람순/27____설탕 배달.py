d5864b364c30582a4c4fc8103c30e50868953fb7
DESKTOP-CVFGNRF\procendp <procendp@gmail.com>
2020-10-12 23:49:51 +0900

    modified BKJNQ27

--- BaekJoon/Python/맞은사람순/27____설탕 배달.py
+++ BaekJoon/Python/맞은사람순/27____설탕 배달.py
@@ -30,19 +30,19 @@ if N % 5 != 0 and N % 3 != 0:
 
 
 
-if N % 5 == 0 or N % 3 == 0:
-    if N % 5 == 0:
-        num.append(N // 5)
-        num.append((N // 5) // 3)
-        num_list = list(map(int, num))
-        total = sum(num_list)
-        print(total)
-    elif N % 5 != 0:
-        print(N // 3)
+# if N % 5 == 0 or N % 3 == 0:
+#     if N % 5 == 0:
+#         num.append(N // 5)
+#         num.append((N // 5) // 3)
+#         num_list = list(map(int, num))
+#         total = sum(num_list)
+#         print(total)
+#     elif N % 5 != 0:
+#         print(N // 3)
 
 
-elif N % 5 != 0 and N % 3 != 0:
-    print(-1)
+# elif N % 5 != 0 and N % 3 != 0:
+#     print(-1)
 
 
 
