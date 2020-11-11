e478936b8d1872ceba73888233647bab9eeacb58
DESKTOP-CVFGNRF\procendp <procendp@gmail.com>
2020-11-12 02:26:13 +0900

    retry

--- Programmers/Python/LV1/_소수 찾기.py
+++ Programmers/Python/LV1/_소수 찾기.py
@@ -32,5 +32,5 @@ print(solution(5))
 #     for i in range(2,n+1):
 #         if i in num:
 #             num -=set(range(2*i,n+1,i))
-#             num[i]
+#             num[i] = 
 #     return len(num)
