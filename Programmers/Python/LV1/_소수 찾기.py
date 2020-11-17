ab3359557d39ac369d5b839b187a7234f8bcc673
DESKTOP-CVFGNRF\procendp <procendp@gmail.com>
2020-11-17 21:28:28 +0900

    completed finding prime

--- Programmers/Python/LV1/_소수 찾기.py
+++ Programmers/Python/LV1/_소수 찾기.py
@@ -1,25 +1,39 @@
-# 재도전
 def solution(n):
-    array = []
-    some = []
-    for i in range(1, n+1): # len(n)
-        array.append(i)     # [1, 2, 3, 4, 5]
-    array.remove(array[0])  # [2, 3, 4, 5]  # 1 본인 제외
+    num_set = set(range(2, n+1))
+    for i in range(2, n+1):
+        if i in num_set:
+            num_set -= set(range(2*i, n+1, i))
 
+    answer = len(num_set)
 
-    for j in range(0, len(array)):
-        # print(j)        # 0  1  2  3
-        # print(array[j]) # 2, 3, 4, 5
-        
-        # 약수
-        for k in range(1, j):
-            if array[j] % k == 0:
-                some.append(array[j])
+    return answer
 
-    return some
-
-# print(solution(10))
+print(solution(10))
 print(solution(5))
+
+
+# 재도전
+# def solution(n):
+#     array = []
+#     some = []
+#     for i in range(1, n+1): # len(n)
+#         array.append(i)     # [1, 2, 3, 4, 5]
+#     array.remove(array[0])  # [2, 3, 4, 5]  # 1 본인 제외
+
+
+#     for j in range(0, len(array)):
+#         # print(j)        # 0  1  2  3
+#         # print(array[j]) # 2, 3, 4, 5
+        
+#         # 약수
+#         for k in range(1, j):
+#             if array[j] % k == 0:
+#                 some.append(array[j])
+
+#     return some
+
+# # print(solution(10))
+# print(solution(5))
 
 
 
