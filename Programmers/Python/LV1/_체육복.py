1d8c18e08828a9c028146ec690523e1fc22bca90
DESKTOP-CVFGNRF\procendp <procendp@gmail.com>
2020-11-25 21:11:54 +0900

    add it

--- Programmers/Python/LV1/_체육복.py
+++ Programmers/Python/LV1/_체육복.py
@@ -6,3 +6,6 @@ def solution(n, lost, reserve):
 print(solution(5, [2,4], [1,3,5]))
 print(solution(5, [2,4], [3]))
 print(solution(3, [3], [1]))
+
+# 예제 #1
+# 1번 학생이 2번 학생에게 체육복을 빌려주고, 3번 학생이나 5번 학생이 4번 학생에게 체육복을 빌려주면 학생 5명이 체육수업을 들을 수 있습니다.
