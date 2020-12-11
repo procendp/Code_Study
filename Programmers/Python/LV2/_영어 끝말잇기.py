c6c8629ee7fbbe062b6bd300612c083608090af5
DESKTOP-CVFGNRF\procendp <procendp@gmail.com>
2020-12-11 21:45:25 +0900

    retry

--- Programmers/Python/LV2/_영어 끝말잇기.py
+++ Programmers/Python/LV2/_영어 끝말잇기.py
@@ -11,6 +11,6 @@ def solution(n, words):
 
 
 
-print(solution(3, ["tank", "kick", "know",    "wheel", "land", "dream",    "mother", "robot", "tank"])) # [3,3]
+print(solution(3, ["tank", "kick", "know",    "wheel", "land", "dream",    "mother", "robot", "tank"]))# [3,3]
 print(solution(5, ["hello", "observe", "effect", "take", "either",   "recognize", "encourage", "ensure", "establish", "hang",   "gather", "refer", "reference", "estimate", "executive"])) # [0,0]
 print(solution(2, ["hello", "one",   "even", "never",   "now", "world",   "draw"]))# [1,3]
