5b301765ef86cb6a8d04ce6a3812d2827f285f2f
DESKTOP-CVFGNRF\procendp <procendp@gmail.com>
2020-12-13 23:21:04 +0900

    phoneketmon failed

--- Programmers/Python/LV2/_폰켓몬.py
+++ Programmers/Python/LV2/_폰켓몬.py
@@ -3,6 +3,6 @@ def solution(nums):
 
     return answer
 
-print(solution([3,1,2,3]))
-print(solution([3,3,3,2,2,4]))
-print(solution([3,3,3,2,2,2]))
+print(solution([3,1,2,3]))      # 2
+print(solution([3,3,3,2,2,4]))  # 3
+print(solution([3,3,3,2,2,2]))  # 2
