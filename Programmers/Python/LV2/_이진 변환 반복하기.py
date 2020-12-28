2cfad4efc1bc3e8dfd9161276dede856ff962039
DESKTOP-CVFGNRF\procendp <procendp@gmail.com>
2020-12-28 17:56:00 +0900

    retry when..

--- Programmers/Python/LV2/_이진 변환 반복하기.py
+++ Programmers/Python/LV2/_이진 변환 반복하기.py
@@ -1,20 +1,30 @@
 # 다시
-
 def solution(s):
-    a, b = 0, 0
-
-    while s != '1':
-        a += 1
-        num = s.count('1')
-        b += len(s) - num
-        s = bin(num)[2:]
-
-    return [a, b]
+    answer = list(map(int, s))
+    answer.remove(0)
+    
+    return answer
 
 
-print(solution("110010101001"))
-print(solution("01110"))
-print(solution("1111111"))
+print(solution("110010101001")) # [3,8]
+print(solution("01110")) # [3,3]
+print(solution("1111111")) # [4,1]
+
+# def solution(s):
+#     a, b = 0, 0
+
+#     while s != '1':
+#         a += 1
+#         num = s.count('1')
+#         b += len(s) - num
+#         s = bin(num)[2:]
+
+#     return [a, b]
+
+
+# print(solution("110010101001"))
+# print(solution("01110"))
+# print(solution("1111111"))
 
 
 
--- Programmers/Python/LV2/_최댓값과 최솟값.py
+++ Programmers/Python/LV2/_최댓값과 최솟값.py
@@ -1,17 +1,41 @@
 #  다시
-
 def solution(s):
+    answer = ''
     
-    s = list(map(int, s.split(' ')))
-    
-    return "{} {}".format(min(s), max(s))
 
 
 
-    
+    return answer
+
 print(solution("1 2 3 4"))
 print(solution("-1 -2 -3 -4"))
 print(solution("-1 -1"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def solution(s):
+    
+#     s = list(map(int, s.split(' ')))
+    
+#     return "{} {}".format(min(s), max(s))
+
+
+
+    
+# print(solution("1 2 3 4"))
+# print(solution("-1 -2 -3 -4"))
+# print(solution("-1 -1"))
 
 
 
