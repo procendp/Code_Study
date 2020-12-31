59633474999e0cb49aba679551ccf55f78bf4b1a
DESKTOP-CVFGNRF\procendp <procendp@gmail.com>
2020-12-31 20:55:21 +0900

    re

--- Programmers/Python/LV2/_이진 변환 반복하기.py
+++ Programmers/Python/LV2/_이진 변환 반복하기.py
@@ -1,36 +1,20 @@
 # 다시
+
 def solution(s):
-    answer = list(map(int, s))
-    answer.remove(0)
-    
-    answer.count(1)
-    for i in answer:
-        
+    a, b = 0, 0
+
+    while s != '1':
+        a += 1
+        num = s.count('1')
+        b += len(s) - num
+        s = bin(num)[2:]
+
+    return [a, b]
 
 
-    
-    return answer
-
-
-print(solution("110010101001")) # [3,8]
-print(solution("01110")) # [3,3]
-print(solution("1111111")) # [4,1]
-
-# def solution(s):
-#     a, b = 0, 0
-
-#     while s != '1':
-#         a += 1
-#         num = s.count('1')
-#         b += len(s) - num
-#         s = bin(num)[2:]
-
-#     return [a, b]
-
-
-# print(solution("110010101001"))
-# print(solution("01110"))
-# print(solution("1111111"))
+print(solution("110010101001"))
+print(solution("01110"))
+print(solution("1111111"))
 
 
 
