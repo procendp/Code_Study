ff8d2d0401d7d10268955366938ecbb7a70f7209
DESKTOP-CVFGNRF\procendp <procendp@gmail.com>
2021-01-03 21:09:52 +0900

    retry on sunday

--- Programmers/Python/LV2/_영어 끝말잇기.py
+++ Programmers/Python/LV2/_영어 끝말잇기.py
@@ -1,34 +1,12 @@
 # 다시
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def solution(n, words):
-#     for i in range(1, len(words)):
-#         if words[i][0] != words[i-1][-1] or words[i] in words[:i]: # 뒤에 잘 연결되었는지 or 중복 있나
+def solution(n, words):
+    for i in range(1, len(words)):
+        if words[i][0] != words[i-1][-1] or words[i] in words[:i]: # 뒤에 잘 연결되었는지 or 중복 있나
             
-#             return [(i % n) + 1, (i // n) + 1] # -번 사람, -번째 워드
-#     else:
-#         return [0,0]
+            return [(i % n) + 1, (i // n) + 1] # -번 사람, -번째 워드
+    else:
+        return [0,0]
 
 
 
--- Programmers/Python/LV2/_행렬의 곱셈.py
+++ Programmers/Python/LV2/_행렬의 곱셈.py
@@ -1,19 +1,14 @@
 # 다시
-# numpy 없이는?
+
+import numpy as np
+
+def solution(arr1, arr2):
+
+    return (np.matrix(arr1) * np.matrix(arr2)).tolist()
 
 
-
-
-
-# import numpy as np
-
-# def solution(arr1, arr2):
-
-#     return (np.matrix(arr1) * np.matrix(arr2)).tolist()
-
-
-# print(solution([[1, 4], [3, 2], [4, 1]], [[3, 3], [3, 3]]))
-# print(solution([[2, 3, 2], [4, 2, 4], [3, 1, 4]], [[5, 4, 3], [2, 4, 1], [3, 1, 1]]))
+print(solution([[1, 4], [3, 2], [4, 1]], [[3, 3], [3, 3]]))
+print(solution([[2, 3, 2], [4, 2, 4], [3, 1, 4]], [[5, 4, 3], [2, 4, 1], [3, 1, 1]]))
 
 
 
@@ -38,3 +33,7 @@
 2 4 1
 3 1 1
 '''
+
+
+
+# numpy 없이는?
