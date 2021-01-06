63ff7b48a578817eed415c63963ae85c2117d6b5
DESKTOP-CVFGNRF\procendp <procendp@gmail.com>
2021-01-06 21:53:58 +0900

    ret

--- Programmers/Python/LV2/_방금그곡.py
+++ Programmers/Python/LV2/_방금그곡.py
@@ -1,13 +1,13 @@
-def solution(m, musicinfos):
-    answer = ''
-    sound = "C, C#, D, D#, E, F, F#, G, G#, A, A#, B"
+# def solution(m, musicinfos):
+#     answer = ''
+#     sound = "C, C#, D, D#, E, F, F#, G, G#, A, A#, B"
 
-    return answer
+#     return answer
 
-print(solution("ABCDEFG", ["12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF"])) # "HELLO"
-print(solution("CC#BCC#BCC#BCC#B", ["03:00,03:30,FOO,CC#B", "04:00,04:08,BAR,CC#BCC#BCC#B"])) # "FOO"
-print(solution("ABC", ["12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"])) # "WORLD"
-# 추가 테스트 케이스
-print(solution("A#",["23:56,00:00,HAPPY,B#A#"])) #happy
-print(solution("CDEFGAC",["12:00,12:06,HELLO,CDEFGA"])) #none
-print(solution("CCB",["03:00,03:10,FOO,CCB#CCB", "04:00,04:08,BAR,ABC"])) #foo
+# print(solution("ABCDEFG", ["12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF"])) # "HELLO"
+# print(solution("CC#BCC#BCC#BCC#B", ["03:00,03:30,FOO,CC#B", "04:00,04:08,BAR,CC#BCC#BCC#B"])) # "FOO"
+# print(solution("ABC", ["12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"])) # "WORLD"
+# # 추가 테스트 케이스
+# print(solution("A#",["23:56,00:00,HAPPY,B#A#"])) #happy
+# print(solution("CDEFGAC",["12:00,12:06,HELLO,CDEFGA"])) #none
+# print(solution("CCB",["03:00,03:10,FOO,CCB#CCB", "04:00,04:08,BAR,ABC"])) #foo
--- Programmers/Python/LV2/_행렬의 곱셈.py
+++ Programmers/Python/LV2/_행렬의 곱셈.py
@@ -1,14 +1,55 @@
 # 다시
-
-import numpy as np
+# numpy 없이는?
 
 def solution(arr1, arr2):
+    answer = [[]]
 
-    return (np.matrix(arr1) * np.matrix(arr2)).tolist()
+    return answer
+
+
+
+
+
+
+
+
+
+
+
 
 
 print(solution([[1, 4], [3, 2], [4, 1]], [[3, 3], [3, 3]]))
 print(solution([[2, 3, 2], [4, 2, 4], [3, 1, 4]], [[5, 4, 3], [2, 4, 1], [3, 1, 1]]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import numpy as np
+
+# def solution(arr1, arr2):
+
+#     return (np.matrix(arr1) * np.matrix(arr2)).tolist()
+
+
+# print(solution([[1, 4], [3, 2], [4, 1]], [[3, 3], [3, 3]]))
+# print(solution([[2, 3, 2], [4, 2, 4], [3, 1, 4]], [[5, 4, 3], [2, 4, 1], [3, 1, 1]]))
 
 
 
@@ -36,4 +77,3 @@ print(solution([[2, 3, 2], [4, 2, 4], [3, 1, 4]], [[5, 4, 3], [2, 4, 1], [3, 1, 
 
 
 
-# numpy 없이는?
