22b1bc133cf245d4baaa58e20fd6a8d6962a9197
DESKTOP-CVFGNRF\procendp <procendp@gmail.com>
2021-01-08 21:54:57 +0900

    complete

---
+++ Programmers/Python/LV2/최댓값과 최솟값.py
@@ -0,0 +1,10 @@
+def solution(s):
+    s = list(map(int, s.split(' ')))
+
+    return "{} {}".format(min(s), max(s))
+
+
+
+print(solution("1 2 3 4"))
+print(solution("-1 -2 -3 -4"))
+print(solution("-1 -1"))
