688b2803d1170d8707c27b803a342a20be4edf69
DESKTOP-CVFGNRF\procendp <procendp@gmail.com>
2021-01-09 19:56:05 +0900

    modified

--- Programmers/Python/LV2/_가장 큰 수.py
+++ Programmers/Python/LV2/_가장 큰 수.py
@@ -1,17 +1,27 @@
 # 다시
-
 def solution(numbers):
+    answer = ''
 
-    numbers = list(map(str, numbers))
-    numbers.sort(key= lambda x : x*3, reverse= True)
-
-    answer = "".join(map(str, numbers))
 
     return answer
 
-
 print(solution([6, 10, 2]))
 print(solution([3, 30, 34, 5, 9]))
+
+
+
+# def solution(numbers):
+
+#     numbers = list(map(str, numbers))
+#     numbers.sort(key= lambda x : x*3, reverse= True)
+
+#     answer = "".join(map(str, numbers))
+
+#     return answer
+
+
+# print(solution([6, 10, 2]))
+# print(solution([3, 30, 34, 5, 9]))
 
 
 
