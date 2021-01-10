f20e2aa3cabc14b536a5100f2bb0d9995a4e758b
DESKTOP-CVFGNRF\procendp <procendp@gmail.com>
2021-01-10 21:47:59 +0900

    retry

--- Programmers/Python/LV2/_가장 큰 수.py
+++ Programmers/Python/LV2/_가장 큰 수.py
@@ -1,9 +1,13 @@
 # 다시
 def solution(numbers):
-    answer = ''
+    numbers = list(map(str, numbers)) # int(numbers) -> str
+    somelist = []
+
+    for i in range(len(numbers)):
+        somelist.append(numbers[i] + numbers[i-1])
 
 
-    return answer
+    return somelist
 
 print(solution([6, 10, 2]))
 print(solution([3, 30, 34, 5, 9]))
