b910ea0778c7a1231a6b170f31bcdf70145d5ba9
DESKTOP-CVFGNRF\procendp <procendp@gmail.com>
2021-01-20 21:45:37 +0900

    rererereererererere

--- Programmers/Python/LV2/_124 나라의 숫자.py
+++ Programmers/Python/LV2/_124 나라의 숫자.py
@@ -1,20 +1,7 @@
 # 다시
-
-
-# 잘못품
 def solution(n):
     answer = ""
-    if n % 3 == 1:
-        answer = "1"
-        return answer
-
-    elif n % 3 == 2:
-        answer = "2"
-        return answer
-
-    elif n % 3 == 0:
-        answer = "4"
-        return answer
+   
 
     return answer
 
@@ -22,3 +9,47 @@ print(solution(1)) # 1
 print(solution(2)) # 2
 print(solution(3)) # 4
 print(solution(4)) # 11
+print(solution(11)) # 42
+
+
+
+# 잘못 품
+# def solution(n):
+#     answer = ""
+#     if n % 3 == 1:
+#         answer = "1"
+#         return answer
+
+#     elif n % 3 == 2:
+#         answer = "2"
+#         return answer
+
+#     elif n % 3 == 0:
+#         answer = "4"
+#         return answer
+
+#     return answer
+
+# print(solution(1)) # 1
+# print(solution(2)) # 2
+# print(solution(3)) # 4
+# print(solution(4)) # 11
+
+
+
+
+1    1
+2    2
+3    4
+4   11
+5   12
+
+6   14
+7   21
+8   22
+9   24
+10  41
+
+11  42
+12  44
+13  111 ...
